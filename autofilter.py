ed9d002531c56ab5d5f649ebfb92bcc0d36427d4
Shinzzo <nikoszabakikas@gmail.com>
2023-11-06 20:08:27 +0200

    near to finish, Version: 0.8 ~Untested!

--- autofilter.py
+++ autofilter.py
@@ -1,5 +1,7 @@
-"""@Author: Shinzzo"""
-   
+#
+#   @Author: Shinzzo
+#
+
 #   Useful Modules.
 import os,collections
 
@@ -10,29 +12,43 @@ images  = ('png', 'jpg', 'jpeg', 'gif', 'svg', 'bmp', 'svg',)
 documents  = ('txt', 'pdf', 'doc', 'docx', 'html', 'ppt', 'pptx', 'log')
 compressed = ('zip', '7z', 'rar', 'tar', 'gz', 'rpm', 'pkg', 'deb')
 executables = ('dmg', 'exe', 'iso')
-
-
-
     
-
-#   Main Function that creates directories.
+#   Main Function that creates directories & maps files from downloads folder based on their file extensions.
 def main():
     cwd = os.path.expanduser('~')
-    DEST_DIRS = ('Music', 'Movies', 'Pictures', 'Documents', 'Applications', 'Other')
-
-    for d in DEST_DIRS:
+    destindirs = ('Music', 'Movies', 'Pictures', 'Documents', 'Applications', 'Other')
+    for d in destindirs:
         dir_path = os.path.join(cwd, d)
         if not os.path.isdir(dir_path):
             os.mkdir(dir_path)
-
+    #--------------------------------------------------------#
     DOWNLOADS_PATH = os.path.join(cwd, 'Downloads')
     files_mapping = collections.defaultdict(list)
     files_list = os.listdir(DOWNLOADS_PATH)
-
     for file_name in files_list:
         if file_name[0] != '.':
             file_ext = file_name.split('.')[-1]
             files_mapping[file_ext].append(file_name)
+    #--------------------------------------------------------#
+    for f_ext, f_list in files_mapping.items():
+        if f_ext in executables:
+            for file in f_list:
+                os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(cwd, 'Applications', file))
+        elif f_ext in audio:
+            for file in f_list:
+                os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(cwd, 'Music', file))
+        elif f_ext in video:
+            for file in f_list:
+                os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(cwd, 'Movies', file))
+        elif f_ext in images:
+            for file in f_list:
+                os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(cwd, 'Pictures', file))
+        elif f_ext in documents or f_ext in compressed:
+            for file in f_list:
+                os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(cwd, 'Documents', file))
+        else:
+            for file in f_list:
+                os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(cwd, 'Other', file))
 
 #   Starts the script.            
 if __name__=="__main__":
