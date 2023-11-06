15ce295ddbf2895e8d0b82224a19515a02f9b865
Shinzzo <nikoszabakikas@gmail.com>
2023-11-06 18:37:02 +0200

    first version of autofilter.

---
+++ autofilter.py
@@ -0,0 +1,8 @@
+import os
+
+audio = ['mp3', 'wav', 'midi']
+video = ['mp4', 'mpg', 'mpeg', 'avi', 'mov', 'flv', 'mkv', 'mwv', 'm4v', 'h264']
+images  = ['png', 'jpg', 'jpeg', 'gif', 'svg', 'bmp', 'psd', 'svg', 'tiff', 'tif']
+documents  = ['txt', 'pdf', 'csv', 'xls', 'xlsx', 'ods', 'doc', 'docx', 'html', 'odt', 'tex', 'ppt', 'pptx', 'log']
+compressed = ['zip', 'z', '7z', 'rar', 'tar', 'gz', 'rpm', 'pkg', 'deb']
+executables = ['dmg', 'exe', 'iso']
