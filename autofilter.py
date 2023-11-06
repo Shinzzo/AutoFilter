c374f5d2a2944a520b7c963c9c1d5994804bb4b4
Shinzzo <nikoszabakikas@gmail.com>
2023-11-06 20:01:35 +0200

    hotfix

--- autofilter.py
+++ autofilter.py
@@ -11,14 +11,29 @@ documents  = ('txt', 'pdf', 'doc', 'docx', 'html', 'ppt', 'pptx', 'log')
 compressed = ('zip', '7z', 'rar', 'tar', 'gz', 'rpm', 'pkg', 'deb')
 executables = ('dmg', 'exe', 'iso')
 
-#   Main Function that creates directories and calls other functions.
+
+
+    
+
+#   Main Function that creates directories.
 def main():
-    BASE_PATH = os.path.expanduser('~')
+    cwd = os.path.expanduser('~')
     DEST_DIRS = ('Music', 'Movies', 'Pictures', 'Documents', 'Applications', 'Other')
 
     for d in DEST_DIRS:
-        dir_path = os.path.join(BASE_PATH, d)
+        dir_path = os.path.join(cwd, d)
         if not os.path.isdir(dir_path):
             os.mkdir(dir_path)
+
+    DOWNLOADS_PATH = os.path.join(cwd, 'Downloads')
+    files_mapping = collections.defaultdict(list)
+    files_list = os.listdir(DOWNLOADS_PATH)
+
+    for file_name in files_list:
+        if file_name[0] != '.':
+            file_ext = file_name.split('.')[-1]
+            files_mapping[file_ext].append(file_name)
+
+#   Starts the script.            
 if __name__=="__main__":
     main()
