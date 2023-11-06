46f20e9fd7aefe73ba8f5e10d35288ddb63461e1
Shinzzo <nikoszabakikas@gmail.com>
2023-11-06 19:11:20 +0200

    renamed file, added comments and modules

--- autofilter.py
+++ autofilter.py
@@ -1,8 +1,13 @@
-import os
+"""@Author: Shinzzo"""
+   
+#   Useful Modules
+import os,collections
 
+#   Some Useful File Extensions.
 audio = ['mp3', 'wav', 'midi']
-video = ['mp4', 'mpg', 'mpeg', 'avi', 'mov', 'flv', 'mkv', 'mwv', 'm4v', 'h264']
-images  = ['png', 'jpg', 'jpeg', 'gif', 'svg', 'bmp', 'psd', 'svg', 'tiff', 'tif']
-documents  = ['txt', 'pdf', 'csv', 'xls', 'xlsx', 'ods', 'doc', 'docx', 'html', 'odt', 'tex', 'ppt', 'pptx', 'log']
-compressed = ['zip', 'z', '7z', 'rar', 'tar', 'gz', 'rpm', 'pkg', 'deb']
+video = ['mp4', 'mpg', 'mpeg', 'avi', 'mov', 'flv', 'mkv', 'm4v', 'h264']
+images  = ['png', 'jpg', 'jpeg', 'gif', 'svg', 'bmp', 'svg',]
+documents  = ['txt', 'pdf', 'doc', 'docx', 'html', 'ppt', 'pptx', 'log']
+compressed = ['zip', '7z', 'rar', 'tar', 'gz', 'rpm', 'pkg', 'deb']
 executables = ['dmg', 'exe', 'iso']
+
