792f69e522d387ddd33f539dd50c5d946f08b778
Shinzzo <nikoszabakikas@gmail.com>
2023-11-06 19:30:54 +0200

    fixes modules

--- autofilter.py
+++ autofilter.py
@@ -1,13 +1,14 @@
 """@Author: Shinzzo"""
    
 #   Useful Modules
-import os,collections
+import os
+import collections
 
 #   Some Useful File Extensions.
-audio = ['mp3', 'wav', 'midi']
-video = ['mp4', 'mpg', 'mpeg', 'avi', 'mov', 'flv', 'mkv', 'm4v', 'h264']
-images  = ['png', 'jpg', 'jpeg', 'gif', 'svg', 'bmp', 'svg',]
-documents  = ['txt', 'pdf', 'doc', 'docx', 'html', 'ppt', 'pptx', 'log']
-compressed = ['zip', '7z', 'rar', 'tar', 'gz', 'rpm', 'pkg', 'deb']
-executables = ['dmg', 'exe', 'iso']
+audio = ('mp3', 'wav', 'midi')
+video = ('mp4', 'mpg', 'mpeg', 'avi', 'mov', 'flv', 'mkv', 'm4v', 'h264')
+images  = ('png', 'jpg', 'jpeg', 'gif', 'svg', 'bmp', 'svg',)
+documents  = ('txt', 'pdf', 'doc', 'docx', 'html', 'ppt', 'pptx', 'log')
+compressed = ('zip', '7z', 'rar', 'tar', 'gz', 'rpm', 'pkg', 'deb')
+executables = ('dmg', 'exe', 'iso')
 
